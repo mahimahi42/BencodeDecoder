59e477ca0ebbf348c25942b071bab9a912403248
Bryce Davis <BAD1128@gmail.com>
2013-10-15 11:59:22 -0400

    Add implementation file with parser method stubs

---
+++ Bencode.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <assert.h>
+#include <ctype.h>
+
+#include "Bencode.h"
+
+char* parse_bencode_string(char* be_str) {
+
+}
+
+char* parse_bencode_integer(char* be_str) {
+
+}
