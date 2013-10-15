5707529365097b0f25f7b9c912d87c9b300da5ab
Bryce Davis <BAD1128@gmail.com>
2013-10-15 12:50:21 -0400

    Working implementation of parse string

--- Bencode.c
+++ Bencode.c
@@ -1,35 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <assert.h>
 #include <ctype.h>
 
 #include "Bencode.h"
 
+char* get_substring(const char* str, int beg, int n) {
+    assert(beg - 1+n < strlen(str));
+    char* ret = malloc(n + 1);
+    strncpy(ret, (str + beg), n);
+    *(ret+n) = 0;
+
+    return ret;
+}
+
 char* parse_bencode_string(char* be_str) {
     int i;
-    int str_len;
-    for (i = 0; i < (int)strlen(be_str); i++) {
-        if (isdigit(be_str[i])) {
-            int j = i;
-            while (isdigit(be_str[i])) {
-                j++;
-            }
-            if (be_str[j] == ':') {
-                char buf[j - i + 1];
-                memcpy(buf, &be_str[i], j - i);
-                buf[j - i] = '\0';
-                str_len(buf, NULL, 10);
-                char ret_str[str_len + 1];
-                memcpy(ret_str, &be_str[j + 1], str_len);
-                ret_str[str_len - 1] = '\0';
-                return ret_str;
-            }
+    int len_str;
+    sscanf(be_str, "%i", &len_str);
+    for (i = 0; i < strlen(be_str); i++) {
+        if (be_str[i - 1] == ':') {
+            break;
         }
     }
-    char* ret_str = "";
+    char* ret_str = get_substring(be_str, i, len_str);
+    strncpy(ret_str, &be_str[i], len_str);
     return ret_str;
 }
 
 char* parse_bencode_integer(char* be_str) {
+    return 0;
+}
 
+int main() {
+    char* test = "4:spam";
+    /*const char* res = parse_bencode_string(test);
+    puts(res);*/
+    printf("%s\n", parse_bencode_string(test));
+    return 0;
 }
--- Bencode.h
+++ Bencode.h
@@ -1,7 +1,7 @@
 #ifndef BENCODE_H
 #define BENCODE_H
 
-string parse_bencode_string(char* be_str);
+char* parse_bencode_string(char* be_str);
 char* parse_bencode_integer(char* be_str);
 
 #endif
